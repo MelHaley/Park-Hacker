9040024ba01216e33fb4dbcac91199ccc002bb7e
Mel Haley <mel.haley@gmail.com>
2023-11-09 14:24:42 -0500

    cleaned-up code, added holiday_dict

--- ph-demo.py
+++ ph-demo.py
@@ -9,8 +9,6 @@ import user_predictor as up
 # load parks dataframe
 parks_df = pd.read_csv('data/parks_df.csv')
 
-
-
 # streamlit app
 
 st.image('background.png', caption='Image by macrovector on Freepik') 
@@ -31,6 +29,7 @@ date_choice = st.date_input(
 time_choice = st.time_input('When will you start your day?', datetime.time(9, 0))
 
 # ride choices
+ride_choices = []
 if park_choice == 'Animal Kingdom':
     ride_choices = st.multiselect(
         'What rides do you want to go on? (pick 2 minimum)',
@@ -50,7 +49,7 @@ elif park_choice == 'Magic Kingdom':
        
 
 def hour_min(minutes):
-    '''function to convert minutes to hour/minutes'''
+    """function to convert minutes to hour/minutes"""
     return "%02d:%02d " % (divmod(minutes, 60))
  
     
@@ -62,36 +61,33 @@ user_input = {'park': park_choice,
 
      
 # create features from user input
-df_X = up.input_df(user_input)
-
+if len(ride_choices) >= 2:
+    df_x = up.input_df(user_input)
 
 # get predictions
-if len(ride_choices) >= 2:
+    ride_pred = pd.DataFrame()
     ride_list = []
     for ride in ride_choices:
         ride_list.append(parks_df['ride'].loc[(parks_df['short_name'] == ride)].item())
-    ride_pred = up.get_predictions(ride_list, df_X) 
-  
+    ride_pred = up.get_predictions(ride_list, df_x) 
 
 # get itinerary and comparison
-if len(ride_choices) >= 2:
-    itinerary = up.get_itinerary(ride_choices, ride_pred, user_input)
-    alternate = up.get_alternate(ride_choices, ride_pred, user_input)
+    itinerary = up.get_itinerary(ride_pred, user_input)
+    alternate = up.get_alternate(ride_pred, user_input)
   
     if itinerary:
         st.write(f"Your Itinerary for {park_choice} on {date_choice} starting at {time_choice}:")
-        #total_time = 0
-        #compared_to = 0
+        # total_time = 0
+        # compared_to = 0
         for i in range(len(ride_choices)):
             st.write(f"{i+1}. {hour_min(itinerary[0][i][2])} | {  itinerary[0][i][0]}   (approx. wait = {int(itinerary[0][i][1])}mins)")
         total_time = int(itinerary[1])
         compared_to = int(alternate[1])
         saved = compared_to - total_time
         st.write(f"Total time: {hour_min(total_time)}")
-   
     
-if st.button("compare"):
-    st.write(f"compared to {hour_min(compared_to)}=>  {hour_min(saved)} mins saved!")
+        if st.button("compare"):
+            st.write(f"compared to {hour_min(compared_to)}=>  {hour_min(saved)} mins saved!")
         
         
         
