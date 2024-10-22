9407d8432ecddb379047db0991ef99876bc48074
Mel Haley <mel.haley@gmail.com>
2024-10-22 18:35:45 -0400

    add text to app

--- ph-demo.py
+++ ph-demo.py
@@ -14,6 +14,9 @@ parks_df = pd.read_csv('data/parks_df.csv')
 st.image('background.png', caption='Image by macrovector on Freepik') 
 st.title('**Park Hacker**')
 st.subheader('_navigate the most efficient route through a theme park_')
+st.write("Every year, many families and groups plan vacations that are centered around visiting theme parks. While various theme parks have different assortments of things to do, one thing that they unfortunately all have in common is long, boring lines.")
+st.write("Current ride wait time data is available via park-specific apps, but they are of limited use in planning your trip or if you'd rather not spend your day at the park looking up wait times on your phone.")
+st.write("With ParkHacker, you simply enter the park you wish to visit, the day of your visit, the time you plan to arrive at the park, and the rides you want to go on. In return, Park Hacker will output a customized itinerary to minimize wait times!")
 
 # park choice
 park_choice = st.radio(
